27bc3381a51cf58272f16eecf49ccd0a626941f5
Choi Jinkyu <kyuparfum@naver.com>
2023-05-10 09:07:21 +0900

    new commit

--- fiiiiilm/urls.py
+++ fiiiiilm/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
-    path('reviews/', include('reviews.urls')),
+    path('', include('reviews.urls')),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from reviews import views
 
 urlpatterns = [
+    path('api/v1/movie', views.MovieApiMain.as_view(), name='main_api'),# 영화api 임시
+    path('api/v1/movie/detail', views.MovieApiDetail.as_view(), name='detail_api'),# 영화api 임시
     path('api/v1/reviews', views.ReviewList.as_view(), name='review_list'),
-    path('api/v1/movie', views.MovieApi.as_view(), name='movie_api'),  # 영화api 임시
-    path('api/v1/reviews/recent',
-         views.ReviewListRecent.as_view(), name='review_recent'),
-    path('api/v1/reviews/<int:pk>',
-         views.ReviewListDetail.as_view(), name='review_detail'),
-    path('/api/v1/reviews/<int:pk>/comments',
-         views.CommentList.as_view(), name='comment_list'),
-    path('/api/v1/comments/<int:pk>/',
-         views.CommentDetail.as_view(), name='comment_detail'),
+    path('api/v1/reviews/recent',views.ReviewListRecent.as_view(), name='review_recent'),
+    path('api/v1/reviews/<int:pk>',views.ReviewListDetail.as_view(), name='review_detail'),
+    path('/api/v1/reviews/<int:pk>/comments',views.CommentList.as_view(), name='comment_list'),
+    path('/api/v1/comments/<int:pk>/',views.CommentDetail.as_view(), name='comment_detail'),
 ]
