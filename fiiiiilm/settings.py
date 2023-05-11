7f0230e0ff1c7426bc9011d9f4411e8ac5fe03c3
Choi Seungwon <cso6549@naver.com>
2023-05-11 11:32:32 +0900

    edit environ

--- fiiiiilm/settings.py
+++ fiiiiilm/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, dotenv
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.read_dotenv((os.path.join(BASE_DIR, '.env')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
