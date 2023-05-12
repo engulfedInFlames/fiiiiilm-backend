9cebf57183a5917e78980f2f68babae4b0e444a4
Choi Jinkyu <kyuparfum@naver.com>
2023-05-12 19:57:12 +0900

    test code 작성

--- reviews/tests.py
+++ reviews/tests.py
@@ -13,14 +13,12 @@ from reviews.serializers import ReviewSerializer
 class ReviewsCreateTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user_data = {"email": "kyu@mail.com", "password":"123"}
-        cls.review_data = {"title":"kyu title","content":"내요오오옹","movie_code":2222222}
-        cls.user = User.objects.create_user("kyu@mail.com","123")
+        cls.user_data = {"email": "kyu@mail.com", "password":"123","nickname":"nick"}
+        cls.review_data = {"title":"kyu title","content":"내요오오옹","movie_code":222222}
+        cls.user = User.objects.create_user("kyu@mail.com","123","nick")
 
     def setUp(self):
         self.access_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["access"]
-
-
 
     def test_fail_if_not_logged_in(self):
         url = reverse("review_list")
@@ -36,4 +34,19 @@ class ReviewsCreateTest(APITestCase):
         self.assertEqual(response.data["message"],"작성완료")
         self.assertEqual(response.status_code, 200)
 
+class ReviewReadTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.faker = Faker()
+        cls.reviews = []
+        for i in range(10):
+            cls.user = User.objects.create_user(cls.faker.email(),cls.faker.word(),cls.faker.name())
+            cls.reviews.append(Review.objects.create(title=cls.faker.sentence(),content=cls.faker.text(),user=cls.user))
 
+    def test_get_review(self):
+        for review in self.reviews:
+            url = review.get_absolute_url()
+            response = self.client.get(url)
+            serializer = ReviewSerializer(review).data
+            for key, value in serializer.items():
+                self.assertEqual(response.data[key], value)
--- users/models.py
+++ users/models.py
@@ -5,14 +5,14 @@ from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, nickname=None):
         if not email:
             raise ValueError("Users must have an email address")
 
         user = self.model(
             email=self.normalize_email(email),
         )
-
+        user.nickname = nickname
         user.set_password(password)
         user.save(using=self._db)
         return user
